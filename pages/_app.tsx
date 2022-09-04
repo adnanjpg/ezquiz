c59343d43acdfc15faa734300fa1b969ca7bb3be
adnan <adnanfahedhr@gmail.com>
2022-09-04 16:10:52 +0300

    init trpc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
+import { withTRPC } from '@trpc/next';
+import { AppRouter } from './api/trpc/[trpc]';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,4 +13,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+    return {
+      url,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,24 @@
+
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Quiz from '../features/quiz/comps/Quiz'
+
+import { trpc } from '../utils/trpc'
+
+// export default function IndexPage() {
+//   const hello = trpc.useQuery(['hello', { text: 'client' }])
+//   if (!hello.data) {
+//     return <div>Loading...</div>
+//   }
+
+//   return (
+//     <div>
+//       <p>{hello.data.greeting}</p>
+//     </div>
+//   )
+// }
+
+
 
 const Home: NextPage = () => {
   return (
