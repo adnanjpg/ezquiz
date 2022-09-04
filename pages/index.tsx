136e8fd01d87b5d337032a6a718eb1c8bb6888b7
adnan <adnanfahedhr@gmail.com>
2022-09-04 19:59:32 +0300

    call trpc in fe and add demo quizzes to ensure it works

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,75 @@ import Quiz from '../features/quiz/comps/Quiz'
 
 import { trpc } from '../utils/trpc'
 
-// export default function IndexPage() {
-//   const hello = trpc.useQuery(['hello', { text: 'client' }])
-//   if (!hello.data) {
-//     return <div>Loading...</div>
-//   }
+export default function IndexPage() {
+  const mutation = trpc.useMutation(['quiz.create'])
 
+
+
+  // if (!hello.data) {
+  //   return <div>Loading...</div>
+  //}
+
+  const onClick = () => {
+    mutation.mutate({
+      questions: [
+        {
+          text: 'lorem ipsum',
+          answers: [
+            {
+              text: 'dolor sit amet',
+              iscorrect: true,
+            },
+            {
+              text: 'consectetur adipiscing elit',
+              iscorrect: false,
+            },
+            {
+              text: 'Aenean eleifend',
+              iscorrect: true,
+            },
+          ]
+        },
+        {
+          text: 'dolor sit amet',
+          answers: [
+            {
+              text: 'consectetur adipiscing elit',
+              iscorrect: false,
+            },
+            {
+              text: 'Aenean eleifend',
+              iscorrect: true,
+            },
+            {
+              text: 'ex ac ullamcorper malesuada,',
+              iscorrect: false,
+            },
+          ]
+        }
+      ]
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={onClick} disabled={mutation.isLoading}>Add Quiz</button>
+      {mutation.data && <p>Last Added quiz id: {mutation.data.id}</p>}
+      {mutation.error && <p>Something went wrong! {mutation.error.message}</p>}
+    </div>
+
+    //   <p>{hello.data.greeting}</p>
+  )
+}
+
+
+
+// const Home: NextPage = () => {
 //   return (
-//     <div>
-//       <p>{hello.data.greeting}</p>
+//     <div className="App">
+//       <Quiz></Quiz>
 //     </div>
 //   )
 // }
 
-
-
-const Home: NextPage = () => {
-  return (
-    <div className="App">
-      <Quiz></Quiz>
-    </div>
-  )
-}
-
-export default Home
+// export default Home
