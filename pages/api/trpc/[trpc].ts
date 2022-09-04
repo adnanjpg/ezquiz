3e5d62a1b68792525679bb6b4a4818e7b902cf5b
adnan <adnanfahedhr@gmail.com>
2022-09-04 17:26:58 +0300

    init quizzes create router

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,21 +1,36 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
+import { createContext, createRouter } from './Context';
 
-export const appRouter = trpc
-    .router()
-    .query('hello', {
-        input: z
-            .object({
-                text: z.string().nullish(),
-            })
-            .nullish(),
-        resolve({ input }) {
+
+const quizzes = createRouter()
+    .mutation('create', {
+        input: z.object({
+            questions: z.array(
+                z.object({
+                    text: z.string(),
+                    answers: z.array(
+                        z.object({
+                            text: z.string(),
+                            iscorrect: z.boolean()
+                        })
+                    )
+                })
+            )
+        }),
+        resolve: ({ input }) => {
+            // add the quiz to db,
+            // and return the id
             return {
-                greeting: `hello ${input?.text ?? 'world'}`,
-            };
-        },
-    });
+                // id: ...
+            }
+        }
+    })
+
+const appRouter = createRouter()
+    .merge('quiz.', quizzes)
+
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
@@ -23,5 +38,5 @@ export type AppRouter = typeof appRouter;
 // export API handler
 export default trpcNext.createNextApiHandler({
     router: appRouter,
-    createContext: () => null,
+    createContext,
 });
