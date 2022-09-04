045696b6bd1c34b720d9537042012ffca1275985
adnan <adnanfahedhr@gmail.com>
2022-09-04 19:49:53 +0300

    add implementation for creating quizzes

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,8 +1,10 @@
+import { PrismaClient } from '@prisma/client';
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 import { createContext, createRouter } from './Context';
 
+const prisma = new PrismaClient()
 
 const quizzes = createRouter()
     .mutation('create', {
@@ -19,11 +21,44 @@ const quizzes = createRouter()
                 })
             )
         }),
-        resolve: ({ input }) => {
+        resolve: async ({ input }) => {
             // add the quiz to db,
             // and return the id
+
+            // TODO: after initing auth take this 
+            // from incoming request
+            const currentUserId = 1
+
+            const cQuiz = await prisma.quiz.create({
+                data: { creatorId: currentUserId }
+            })
+
+            const cQuizId = cQuiz.id
+
+            input.questions.forEach(async question => {
+                const q = await prisma.question.create({
+                    data: {
+                        quizId: cQuizId,
+                        ...question
+                    }
+                })
+
+                const qid = q.id
+
+                question.answers.forEach(async answer => {
+                    await prisma.questionAnswer.create({
+                        data: {
+                            questionId: qid,
+                            ...answer
+                        }
+                    })
+                })
+            })
+
+
+
             return {
-                // id: ...
+                id: cQuizId
             }
         }
     })
