63bd24a3924398f79702db51d472ffbd8cd4ffa4
adnan <adnanfahedhr@gmail.com>
2022-09-10 21:25:09 +0300

    create-quiz-form: enable adding answers to questions

--- app/hooks.ts
+++ app/hooks.ts
@@ -1,6 +1,12 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import {
+  batch,
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux"
+import type { RootState, AppDispatch } from "./store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppBatch = batch
--- features/create-quiz/comps/CreateQuiz.tsx
+++ features/create-quiz/comps/CreateQuiz.tsx
@@ -1,13 +1,23 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
+
+import { FaTimes } from "react-icons/fa"
+
 import CenterHorizontal from "../../../app/comps/center-horizontal"
 import CenterVertical from "../../../app/comps/center-vertical"
 import JoinComps from "../../../app/comps/join-comps"
-import { useAppDispatch, useAppSelector } from "../../../app/hooks"
+import { useAppBatch, useAppDispatch, useAppSelector } from "../../../app/hooks"
 import {
+  createAnswer,
+  addQuestion,
   CreateQuizStep,
+  selectCanAddAnswer,
   selectCurrentStep,
+  setSelectedQuestionId,
   setStep,
   setTitle as setConfirmedTitle,
+  selectCurrentQuestionAnswers,
+  updateAnswer,
+  removeAnswer,
 } from "../createQuizSlice"
 
 export default () => {
@@ -26,8 +36,12 @@ function QuizTitle() {
   const [title, setSelectedTitle] = useState("")
 
   const onClick = () => {
-    dispatch(setConfirmedTitle(title))
-    dispatch(setStep(CreateQuizStep.creatingQuestions))
+    useAppBatch(() => {
+      dispatch(setConfirmedTitle(title))
+      dispatch(addQuestion({ id: "1" }))
+      dispatch(setSelectedQuestionId("1"))
+      dispatch(setStep(CreateQuizStep.creatingQuestions))
+    })
   }
 
   const isValid = title.length > 3
@@ -57,12 +71,77 @@ function AddQuestions() {
   return (
     <CenterVertical>
       <CenterHorizontal>
-        <input
-          placeholder="Question Title"
-          value={qtitle}
-          onChange={(event) => setqtitle(event.target.value)}
-        ></input>
+        <QuestionTitle />
+        <hr className="w-40 m-4 border-solid" />
+        <Answers />
       </CenterHorizontal>
     </CenterVertical>
+  )
+}
+
+function QuestionTitle() {
+  const [qtitle, setqtitle] = useState("")
+
+  return (
+    <input
+      placeholder="Question Title"
+      value={qtitle}
+      onChange={(event) => setqtitle(event.target.value)}
+    />
+  )
+}
+
+function Answers() {
+  const canAddAnswer = useAppSelector(selectCanAddAnswer)
+
+  const dispatch = useAppDispatch()
+
+  const onClickAddAns = () => {
+    dispatch(createAnswer())
+  }
+
+  const answers = useAppSelector(selectCurrentQuestionAnswers) ?? []
+
+  return (
+    <>
+      <div>Answers</div>
+      <>
+        {answers.map((answer) => (
+          <AnswerItem key={answer.id} id={answer.id}></AnswerItem>
+        ))}
+      </>
+      <>
+        {canAddAnswer && (
+          <button className="primary-button" onClick={onClickAddAns}>
+            Add answer
+          </button>
+        )}
+      </>
+    </>
+  )
+}
+
+function AnswerItem(props: { id: string }) {
+  const id = props.id
+
+  const answers = useAppSelector(selectCurrentQuestionAnswers) ?? []
+  const answer = answers.find((e) => e.id === id)
+
+  if (!answer) throw new Error("INVALID ANSWER")
+
+  const dispatch = useAppDispatch()
+
+  const onChangeUpdateAnswer = (event: ChangeEvent<HTMLInputElement>) =>
+    dispatch(updateAnswer({ ...answer, text: event.target.value }))
+
+  const onClickRemoveAnswer = () => dispatch(removeAnswer(id))
+
+  return (
+    <div>
+      <input onChange={onChangeUpdateAnswer} value={answer.text || ""} />
+      <button onClick={onClickRemoveAnswer}>
+        <FaTimes />
+      </button>
+    </div>
   )
 }
